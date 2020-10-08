850b04bfc4a70a0ea9b7aef08a8e36fb835b0a03
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-08 00:32:39 -0400

    fixed redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
 	resources :songs
-	patch 'songs/:id', to: 'songs#update'
-
 	resources :genres
-	patch 'genres/:id', to: 'genres#update'
-
 	resources :artists
-	patch 'artists/:id', to: 'artists#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
